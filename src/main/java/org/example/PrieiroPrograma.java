ccdf0b9d3a96db52e1f9d2da390168e477391a73
AndrezaSS <andreza.ss@live.com>
2023-03-07 10:02:31 -0300

    first commit

---
+++ src/main/java/org/example/PrieiroPrograma.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import org.example.model.Gato;
+
+public class PrieiroPrograma {
+    public static void main(String[] args) {
+
+        Gato gato = new Gato();
+        System.out.println(gato);
+    }
+}
